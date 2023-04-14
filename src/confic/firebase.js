c9a9911443166ac19317ff11a8d9e2543b366d1d
Kiattichai <gading.5457.00@gmail.com>
2023-04-14 21:09:04 +0700

    first commit

---
+++ src/confic/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"
+import { getStorage } from "firebase/storage"
+
+const firebaseConfig = {
+  //get from firebase
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
